e1d588cf77d75d78b27fda22023f202e4b10020b
MarcinZiabek <marcin@ziabek.com>
2022-07-26 16:30:22 +0200

    Fixed Google Analytics events for SPA

--- docs/.vitepress/theme/GoogleAnalytics.vue
+++ docs/.vitepress/theme/GoogleAnalytics.vue
@@ -3,20 +3,43 @@
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
     gtag('config', 'G-L6Z2378WVB');
   </component>
 </template>
 
-<script>
-export default {
-  mounted() {
-    const plugin = document.createElement("script");
+<script setup>
+import { useRouter } from 'vitepress';
+import { watch } from 'vue';
+import { onMounted } from 'vue';
 
-    plugin.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-L6Z2378WVB");
-    plugin.async = true;
+function addGoogleAnalyticsScript() {
+  const plugin = document.createElement("script");
 
-    document.head.appendChild(plugin);
+  plugin.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-L6Z2378WVB");
+  plugin.async = true;
+
+  document.head.appendChild(plugin);
+}
+
+function registerViewChangeListener() {
+  if (typeof window === 'undefined')
+    return;
+
+  const router = useRouter();
+  watch(() => router.route.data.relativePath, handleViewChange, { immediate: true });
+
+  function handleViewChange(path) {
+    if (!window.ga)
+      return;
+
+    window.ga('set', 'page', path);
+    window.ga('send', 'pageview');
   }
-};
+}
+
+onMounted(() => {
+  addGoogleAnalyticsScript();
+  registerViewChangeListener();
+});
+
 </script>
