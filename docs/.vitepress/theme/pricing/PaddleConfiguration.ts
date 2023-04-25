8633f40b264ac06200dc499d0d5c0f76d72b12af
MarcinZiabek <marcin@ziabek.com>
2023-04-26 01:38:14 +0200

    Paddle: enabled production configuration

--- docs/.vitepress/theme/pricing/PaddleConfiguration.ts
+++ docs/.vitepress/theme/pricing/PaddleConfiguration.ts
@@ -1,13 +1,13 @@
-export const PaddleConfiguration = {
-    isProduction: false,
-    vendorId: 11755,
-    professionalLicenseId: 49182,
-    enterpriseLicenseId: 49183,
-};
-
 // export const PaddleConfiguration = {
-//     isProduction: true,
-//     vendorId: 162616,
-//     professionalLicenseId: 823920,
-//     enterpriseLicenseId: 823982,
+//     isProduction: false,
+//     vendorId: 11755,
+//     professionalLicenseId: 49182,
+//     enterpriseLicenseId: 49183,
 // };
+
+export const PaddleConfiguration = {
+    isProduction: true,
+    vendorId: 162616,
+    professionalLicenseId: 823920,
+    enterpriseLicenseId: 823982,
+};
