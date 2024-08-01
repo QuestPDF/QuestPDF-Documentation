5a34ff2b16d9ac6f33bf9660b545f8f42164ebf7
Marcin Ziąbek <marcin@ziabek.com>
2024-08-01 22:20:43 +0200

    Clarified and improved Privacy Policy

--- docs/.vitepress/theme/license/documents/PrivacyPolicy.vue
+++ docs/.vitepress/theme/license/documents/PrivacyPolicy.vue
@@ -10,8 +10,19 @@
       <article>
         <h3>General Rules</h3>
         <p>QuestPDF (Marcin Ziąbek CodeFlint) does not share personal information of any kind with anyone. We will not sell or rent your name or personal information to any third party. We do not sell, rent or provide outside access to our mailing list or any data we store. Any data that a user stores via our facilities is wholly owned by that user or business. At any time a user or business is free to take their data and leave, or to simply delete their data from our facilities.</p>
-        <p>QuestPDF only collects such personal information that is necessary for you to access and use our services. This personal information includes, but is not limited to, first and last name, email address.</p>
+        <p>QuestPDF collects only the personal information necessary to provide product support, which includes data shared during communication via email and other channels. This information is used solely to address your issues during the support process.</p>
         <p>QuestPDF may release personal information if QuestPDF is required to by law, search warrant, subpoena, court order or fraud investigation. We may also use personal information in a manner that does not identify you specifically nor allow you to be contacted but does identify certain criteria about our Site's users in general (such as we may inform third parties about the number of registered users, number of unique visitors, and the pages most frequently browsed).</p>
+      </article>
+
+      <article>
+        <h3>Software</h3>
+        <p>The software does not initiate any network communication nor send data outside of its deployment location. QuestPDF operates on a trust basis. This means it does not check license validity by communicating with QuestPDF servers, does not send any usage information nor metrics, and does not transmit data in any form.</p>
+      </article>
+
+      <article>
+        <h3>Purchases</h3>
+        <p>QuestPDF partners with Paddle, our Merchant of Record, to ensure worldwide tax compliance and secure payment processing. Paddle stores only the information necessary to handle transactions.</p>
+        <p>Paddle adheres to all current data security recommendations. For detailed information on their security practices, please visit: <a href="https://security.paddle.com">Paddle Trust Center</a>.</p>
       </article>
 
       <article>
