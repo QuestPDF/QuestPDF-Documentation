f7f2d373790389c004ba249791c60951c5f44ae2
Marcin Ziąbek <marcin@ziabek.com>
2025-04-04 01:05:21 +0200

    Updated statistics

--- docs/.vitepress/theme/homepage/HomePageStatistics.vue
+++ docs/.vitepress/theme/homepage/HomePageStatistics.vue
@@ -17,12 +17,12 @@ const statistics = [
     {
         icon: "/homepage/stargazers.svg",
         label: "GitHub stargazers",
-        value: "12 600 +"
+        value: "12 650 +"
     },
     {
         icon: "/homepage/downloads.svg",
         label: "NuGet package downloads",
-        value: "8 800 000 +"
+        value: "9 100 000 +"
     }
 ];
 
