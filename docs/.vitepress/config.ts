aca7d98241a22a89409735298e607e94d9f71478
Marcin Ziąbek <marcin@ziabek.com>
2025-05-13 08:53:00 +0200

    Add noIcon property to GitHub and NuGet links in config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -62,8 +62,8 @@ export default defineConfig({
                     },
                 ]
             },
-            { text: 'GitHub', link: 'https://github.com/QuestPDF/QuestPDF' },
-            { text: 'NuGet', link: 'https://www.nuget.org/packages/QuestPDF' },
+            { text: 'GitHub', link: 'https://github.com/QuestPDF/QuestPDF', noIcon: true },
+            { text: 'NuGet', link: 'https://www.nuget.org/packages/QuestPDF', noIcon: true },
             { text: 'Contact', link: '/contact' },
         ],
         sidebar: [
