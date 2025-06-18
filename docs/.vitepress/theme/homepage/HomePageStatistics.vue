0f4661a68424a4d907b88657e3515468cebfd7de
Marcin Ziąbek <marcin@ziabek.com>
2025-06-18 16:08:18 +0200

    Update statistics values in HomePageStatistics component

--- docs/.vitepress/theme/homepage/HomePageStatistics.vue
+++ docs/.vitepress/theme/homepage/HomePageStatistics.vue
@@ -26,13 +26,13 @@ const statistics = [
         icon: "/homepage/github.svg",
         label: "GitHub stargazers",
         link: "https://github.com/QuestPDF/QuestPDF",
-        value: "12 925 +"
+        value: "13 000 +"
     },
     {
         icon: "/homepage/nuget.svg",
         label: "NuGet package downloads",
         link: "https://www.nuget.org/packages/QuestPDF/",
-        value: "10 200 000 +"
+        value: "10 500 000 +"
     }
 ];
 
