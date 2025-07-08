f3df0036642c81dd47f0e6794d2c76c57252174e
Marcin Ziąbek <marcin@ziabek.com>
2025-07-08 18:04:42 +0200

    Update project statistics

--- docs/.vitepress/theme/homepage/HomePageStatistics.vue
+++ docs/.vitepress/theme/homepage/HomePageStatistics.vue
@@ -26,13 +26,13 @@ const statistics = [
         icon: "/homepage/github.svg",
         label: "GitHub stargazers",
         link: "https://github.com/QuestPDF/QuestPDF",
-        value: "13 050 +"
+        value: "13 075 +"
     },
     {
         icon: "/homepage/nuget.svg",
         label: "NuGet package downloads",
         link: "https://www.nuget.org/packages/QuestPDF/",
-        value: "10 600 000 +"
+        value: "10 750 000 +"
     }
 ];
 
