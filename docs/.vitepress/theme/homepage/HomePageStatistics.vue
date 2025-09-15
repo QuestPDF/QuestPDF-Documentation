d4f745088ac8f52201f87c81163e73e0bec7ff3a
Marcin Ziąbek <marcin@ziabek.com>
2025-09-15 21:46:12 +0200

    Update homepage statistics for GitHub stargazers and NuGet package downloads

--- docs/.vitepress/theme/homepage/HomePageStatistics.vue
+++ docs/.vitepress/theme/homepage/HomePageStatistics.vue
@@ -26,13 +26,13 @@ const statistics = [
         icon: "/homepage/github.svg",
         label: "GitHub stargazers",
         link: "https://github.com/QuestPDF/QuestPDF",
-        value: "13 275 +"
+        value: "13 300 +"
     },
     {
         icon: "/homepage/nuget.svg",
         label: "NuGet package downloads",
         link: "https://www.nuget.org/packages/QuestPDF/",
-        value: "11 750 000 +"
+        value: "12 000 000 +"
     }
 ];
 
