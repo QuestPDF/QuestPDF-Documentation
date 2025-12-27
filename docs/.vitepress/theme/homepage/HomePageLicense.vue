ad4c52e45cfbae9ce22e1923fe88d52f4cda7657
Marcin Ziąbek <marcin@ziabek.com>
2025-12-27 12:04:23 +0100

    Refactor HomePageLicense component for improved layout and styling consistency

--- docs/.vitepress/theme/homepage/HomePageLicense.vue
+++ docs/.vitepress/theme/homepage/HomePageLicense.vue
@@ -24,13 +24,13 @@ const SummaryContent = [
         </article>
       </div>
 
-      <div class="license-warning">
+      <div class="license-description">
         Free for individuals, non-profits, all FOSS projects, and organizations under $1M in annual revenue.
       </div>
 
       <div style="display: flex; flex-direction: row; gap: 16px; justify-content: center">
-        <a class="action pricing" href="/license">View Pricing</a>
-        <a class="action license" href="/license/guide">License terms</a>
+        <a class="action pricing-button" href="/license">View Pricing</a>
+        <a class="action" href="/license/guide">License terms</a>
       </div>
 
   </section>
@@ -38,20 +38,13 @@ const SummaryContent = [
 
 <style scoped>
 
-  .description {
-    place-self: center;
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-  }
-
   .license-icon {
     place-self: center;
     width: 64px;
     margin-bottom: 32px;
   }
 
-  .license-warning {
+  .license-description {
     border-radius: 12px;
     padding: 12px 16px;
     color: var(--vp-custom-block-warning-text);
@@ -66,15 +59,15 @@ const SummaryContent = [
     margin-top: 1.25rem;
   }
 
-  @media screen and (max-width: 700px) {
-    .content {
-      grid-template-columns: 1fr;
-      grid-gap: 32px;
-    }
-
+  @media screen and (max-width: 400px) {
     .license-icon {
-      place-self: center;
-      width: 72px;
+      width: 48px;
+    }
+  }
+
+  @media screen and (max-width: 650px) {
+    .sub-header {
+      text-align: justify;
     }
   }
 
@@ -99,21 +92,21 @@ const SummaryContent = [
 
   /* License button */
 
-  .action.pricing {
+  .action.pricing-button {
     background-color: #66BB6A;
     color: var(--vp-button-brand-text);
     font-weight: bold;
   }
 
-  .action.pricing:hover {
+  .action.pricing-button:hover {
     background-color: #4CAF50;
   }
 
-  html.dark .action.pricing {
+  html.dark .action.pricing-button {
     background-color: #4CAF50;
   }
 
-  html.dark .action.pricing:hover {
+  html.dark .action.pricing-button:hover {
     background-color: #66BB6A;
   }
 </style>
