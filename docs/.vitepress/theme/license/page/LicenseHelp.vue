7d7225dd10abfb42ac678b2b5f697611286c69b8
Marcin Ziąbek <marcin@ziabek.com>
2025-12-27 11:47:29 +0100

    Enhance LicenseHelp component with improved messaging and styling

--- docs/.vitepress/theme/license/page/LicenseHelp.vue
+++ docs/.vitepress/theme/license/page/LicenseHelp.vue
@@ -4,23 +4,22 @@
 
 <template>
   <article class="content license-header">
-    <div>
+    <div class="section-header">
       <h2>Do you need help?</h2>
 
-      <p>If you have any questions or need assistance, feel free to contact us.</p>
+      <p style="margin-top: 8px;">Whether you need a direct invoice, a custom quote, or assistance with a Purchase Order, we ensure a smooth transaction process.</p>
 
-      <p style="margin-top: 8px;">For added convenience, we can prepare quotes, accept purchase orders, and create direct invoices.</p>
-
-      <p style="margin-top: 8px;">
-        Additionally, we collaborate with software resellers to offer flexible purchasing options.<br>
-        We recommended the following resellers: <a href="https://www.softwareone.com/" target="_blank">SoftwareOne</a>, <a href="https://www.qbssoftware.com/" target="_blank">QBS Software</a>, and <a href="https://www.bsrsoftware.co.uk/" target="_blank">BSR Software</a>.
-      </p>
-
-      <a class="action primary" href="mailto:contact@questpdf.com" style="margin-top: 24px;">Contact us</a>
+      <a class="action primary" href="mailto:contact@questpdf.com" style="margin-top: 24px;">Contact sales</a>
     </div>
   </article>
 </template>
 
 <style scoped>
+
+p {
+  font-size: 1.125rem;
+  line-height: 2rem;
+  margin-bottom: 12px;
+}
 
 </style>
