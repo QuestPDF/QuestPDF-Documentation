8704b4164c4efe63fa163b2b5e92aac54fcbe92f
Marcin Ziąbek <marcin@ziabek.com>
2025-12-27 11:48:48 +0100

    Revamp LicensePageTiers component with enhanced layout, descriptions, and styling

--- docs/.vitepress/theme/license/page/LicensePageTiers.vue
+++ docs/.vitepress/theme/license/page/LicensePageTiers.vue
@@ -1,36 +1,51 @@
 <template>
   <article class="content">
-    <h2>Available Licenses</h2>
+    <header>
+      <h1>Transparent Licensing. Enterprise Power.</h1>
+
+      <p class="sub-header">
+        QuestPDF empowers innovation with a generous free tier for startups and open-source projects.
+        For established businesses, our commercial licenses ensure legal compliance, priority support, and long-term stability.
+      </p>
+    </header>
 
     <div class="licenses">
-      <section class="license-tier card" v-for="license of licenses">
-        <header>
-          <img class="icon" :src="license.icon" alt="" />
+      <section class="license-tier card" :class="license.name" v-for="license of licenses">
+
+        <div class="popular">
+          POPULAR
+        </div>
+
+
+
+        <header style="display: flex; flex-direction: column; align-items: start;">
+          <div class="tier-name">{{ license.name }}</div>
 
           <div v-if="license.price">
-            <h3>{{ license.name }}</h3>
-            <p class="price">${{ license.price }}</p>
+            <span class="price-value">${{ license.price }}</span>
+            <span class="price-per">per team/year</span>
           </div>
 
           <div v-else>
-            <h3>Community</h3>
-            <p class="price">Free forever</p>
+            <p class="price-value">Free forever</p>
           </div>
+
+          <div>{{ license.description }}</div>
         </header>
 
         <hr>
 
         <div class="details">
           <div v-for="detail of license.details" class="details-item">
-            <img :src="convertLicenseDetailTypeToIcon(detail.type)" width="20" style="margin-top: 1px;" alt="">
-            <p>{{ detail.content }}</p>
+            <img src="/homepage/square-check-lite.svg" width="24" alt="" />
+            <p>{{ detail }}</p>
           </div>
         </div>
 
         <div style="flex-grow: 1"></div>
 
-        <a v-if="license.price" class="action primary" @click="paddle.startCheckout(license.paddlePriceId)">Purchase</a>
-        <a v-else class="action" href="/getting-started.html">Start learning</a>
+        <a v-if="license.price" class="action" @click="paddle.startCheckout(license.paddlePriceId)">Purchase license</a>
+        <a v-else class="action" href="/getting-started.html">Start Creating</a>
       </section>
     </div>
   </article>
@@ -42,90 +57,56 @@ import {usePaddle} from "../../paddle";
 
 const paddle = usePaddle();
 
-enum LicenseDetailType {
-  Feature,
-  Information,
-  Warning
-}
-
 interface License {
   icon: string;
   name: string;
+  description: string;
 
   price: number;
   paddlePriceId: string;
 
-  details: { type: LicenseDetailType, content: string }[];
+  details: string[];
 }
 
 const CommunityLicense: License = {
   icon: "/license/community.svg",
-  name: "Community",
+  name: "community",
+  description: "Ideal for startups, open-source, and individual developers building the next big thing",
   price: null,
   paddlePriceId: null,
   details: [
-    {
-      type: LicenseDetailType.Information,
-      content: "For private and FOSS projects"
-    },
-    {
-      type: LicenseDetailType.Information,
-      content: "For non-profit organizations"
-    },
-    {
-      type: LicenseDetailType.Information,
-      content: "For evaluation purposes"
-    },
-    {
-      type: LicenseDetailType.Warning,
-      content: "For commercial projects of individuals or businesses with less than 1 million USD annual gross revenue"
-    },
+    "All features available",
+    "For individuals and non-profits",
+    "For open-source projects",
+    "Commercial usage allowed for businesses with less than $1M USD annual gross revenue"
   ]
 };
 
 const ProfessionalLicense: License = {
   icon: "/license/professional.svg",
-  name: "Professional",
-  price: 999,
+  name: "professional",
+  description: "Compliance and stability for established small businesses and agencies",
+  price: 999, // Consider clarifying if this is USD in the UI
   paddlePriceId: paddle.professionalLicensePriceId,
   details: [
-    {
-      type: LicenseDetailType.Feature,
-      content: "For commercial projects"
-    },
-    {
-      type: LicenseDetailType.Feature,
-      content: "For organizations with teams with at most 10 software developers"
-    },
-    {
-      type: LicenseDetailType.Feature,
-      content: "Ensures library sustainability"
-    },
+    "Full commercial usage rights",
+    "Covers up to 10 developers",
+    "Includes 1 year of updates & maintenance",
+    "Funds ongoing development"
   ]
 };
 
 const EnterpriseLicense: License = {
   icon: "/license/enterprise.svg",
-  name: "Enterprise",
+  name: "enterprise",
+  description: "Maximum flexibility and priority support for large teams and mission-critical systems",
   price: 1999,
   paddlePriceId: paddle.enterpriseLicensePriceId,
   details: [
-    {
-      type: LicenseDetailType.Feature,
-      content: "For commercial projects"
-    },
-    {
-      type: LicenseDetailType.Feature,
-      content: "For organizations with software development teams of any size"
-    },
-    {
-      type: LicenseDetailType.Feature,
-      content: "Ensures library sustainability"
-    },
-    {
-      type: LicenseDetailType.Feature,
-      content: "Includes prioritized e-mail support"
-    },
+    "Unlimited developers & server deployments",
+    "Dedicated priority e-mail support",
+    "Frictionless company-wide licensing",
+    "Perpetual license with 1 year of updates & maintenance"
   ]
 };
 
@@ -135,24 +116,22 @@ const licenses = [
   EnterpriseLicense
 ];
 
-function convertLicenseDetailTypeToIcon(type: LicenseDetailType) {
-  if (type === LicenseDetailType.Feature)
-    return "/license/tick.svg";
-
-  if (type === LicenseDetailType.Information)
-    return "/license/info.svg";
-
-  if (type === LicenseDetailType.Warning)
-    return "/license/alert.svg";
-
-  throw "Unreachable code";
-}
-
 </script>
 
 <style scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+header .sub-header {
+  text-align: center;
+  max-width: 720px;
+}
+
 .licenses {
-  margin: 0 auto;
+  margin-top: 64px;
 
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(min(325px, 100%), 1fr));
@@ -160,16 +139,49 @@ function convertLicenseDetailTypeToIcon(type: LicenseDetailType) {
 }
 
 .license-tier {
+  padding-top: 48px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   height: 100%;
 }
 
+.license-tier.professional {
+  border: 1px solid #2196F3;
+  filter: drop-shadow(0 16px 16px #2196F322) !important;
+}
+
+.license-tier:not(.professional) .popular {
+  display: none;
+}
+
+.license-tier.professional .popular {
+
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  transform: translateY(-50%);
+  margin: 0 auto;
+  width: fit-content;
+  padding: 4px 12px;
+  border-radius: 128px;
+  background-color: var(--vp-c-bg);
+  border: 1px solid #2196F3;
+  color: #2196F3;
+  font-weight: 500;
+  font-size: 0.875rem;
+  letter-spacing: 1px;
+}
+
+
+
 .license-tier header {
   display: grid;
   grid-template-columns: auto 1fr;
-  grid-gap: 32px;
+  grid-gap: 24px;
 }
 
 .license-tier header img {
@@ -182,9 +194,20 @@ function convertLicenseDetailTypeToIcon(type: LicenseDetailType) {
   margin-bottom: 8px;
 }
 
-.license-tier p.price {
-  line-height: 1.5rem;
-  font-size: 1.2rem;
+.license-tier .price-value {
+  line-height: 2rem;
+  font-size: 2rem;
+
+  color: var(--vp-c-text-1) !important;
+  font-weight: 700;
+}
+
+.license-tier .price-per {
+  margin-top: 8px;
+
+  color: var(--vp-c-text-1) !important;
+  font-weight: 400;
+  margin-left: 8px;
 }
 
 .license-tier a.action {
@@ -205,10 +228,89 @@ function convertLicenseDetailTypeToIcon(type: LicenseDetailType) {
   flex-direction: row;
   gap: 12px;
   align-items: start;
+}
 
-  color: var(--vp-c-text-2);
+.details-item p {
+  color: var(--vp-c-text-1) !important;
   line-height: 1.5rem;
   font-size: 1rem;
+}
+
+
+
+.license-tier.professional {
+  filter: drop-shadow(0 1px 2px rgba(0, 0, 0, 0.1));
+}
+
+
+
+.license-tier .tier-name {
+  border-radius: 128px;
+  padding: 6px 16px;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-size: 0.875rem;
+}
+
+.license-tier.community .tier-name {
+  background-color: #8884;
+  color: #212121;
+}
+
+.license-tier.professional .tier-name {
+  background-color: #2196F3;
+  color: white;
+}
+
+.license-tier.enterprise .tier-name {
+  background-color: #212121;
+  color: white;
+}
+
+html.dark .license-tier.community .tier-name {
+  color: #F5F5F5;
+}
+
+html.dark .license-tier.professional .tier-name {
+  background-color: #1976D2;
+  color: white;
+}
+
+html.dark .license-tier.enterprise .tier-name {
+  background-color: #EEEEEE;
+  color: #212121;
+}
+
+
+.license-tier.professional .action {
+  background-color: #2196F3;
+  color: white;
+}
+
+.license-tier.enterprise .action {
+  background-color: #F5F5F5;
+  color: #212121;
+  border: 1px solid #212121;
+}
+
+html.dark .license-tier.enterprise .action {
+  background-color: #EEEEEE;
+  color: #212121;
+}
+
+
+.license-tier.professional .action:hover {
+  background-color: #42A5F5;
+  color: white;
+}
+
+.license-tier.enterprise .action:hover {
+  background-color: white;
+}
+
+html.dark .license-tier.enterprise .action:hover {
+  background-color: white;
 }
 
 </style>
