d2e0427be88413daebf6930457c702799e36b850
Marcin Ziąbek <marcin@ziabek.com>
2026-01-30 12:24:48 +0100

    Update NuGet downloads value to reflect current statistics

--- docs/.vitepress/theme/statistics.ts
+++ docs/.vitepress/theme/statistics.ts
@@ -9,6 +9,6 @@ export const statistics = [
         icon: "fa-duotone fa-download",
         label: "NuGet downloads",
         link: "https://www.nuget.org/packages/QuestPDF/",
-        value: "15M"
+        value: "16M"
     }
 ];
