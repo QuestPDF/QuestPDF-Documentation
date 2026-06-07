df7b0bb4149a725283255eaa85bb67819c117d2e
MarcinZiabek <marcin@ziabek.com>
2026-06-07 19:44:28 +0200

    Add Insight as a reseller and adjust reseller grid styles

--- docs/.vitepress/theme/license/pricing/LicenseResellers.vue
+++ docs/.vitepress/theme/license/pricing/LicenseResellers.vue
@@ -6,21 +6,28 @@ const resellers = [
     link: "https://www.softwareone.com/",
     src: "software-one.svg",
     size: 40,
-    marginTop: 0
+    marginTop: -2
+  },
+  {
+    name: "Insight",
+    link: "https://www.insight.com/",
+    src: "insight.svg",
+    size: 46,
+    marginTop: 2
   },
   {
     name: "QBS Software",
     link: "https://www.qbssoftware.com/",
     src: "qbs-software.svg",
-    size: 52,
-    marginTop: 0
+    size: 60,
+    marginTop: 4
   },
   {
     name: "SHI",
     link: "https://www.shi.com/",
     src: "shi.svg",
     size: 48,
-    marginTop: -12
+    marginTop: -14
   }
 ];
 
@@ -36,7 +43,7 @@ const resellers = [
 
     <div class="resellers-list">
       <template v-for="reseller of resellers" :key="reseller.name">
-        <a :href="reseller.link">
+        <a :href="reseller.link" target="_blank">
           <img :src="'/license/resellers/' + reseller.src"
               :alt="reseller.name"
               :style="{ height: reseller.size + 'px', marginTop: reseller.marginTop + 'px' }" />
@@ -52,7 +59,7 @@ const resellers = [
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  gap: 64px;
+  gap: 48px 64px;
   justify-content: center;
   align-items: center;
   margin-top: 64px;
